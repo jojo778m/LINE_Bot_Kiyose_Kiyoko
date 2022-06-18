27159036e6c32d4dd77fb4d4666db6459c1862f5
jojo <jojo778m@gmail.com>
2022-06-18 16:19:57 +0900

    modify and complete swiper

--- LINE_bot.js
+++ LINE_bot.js
@@ -9,11 +9,21 @@ window.addEventListener('load', function(){
   node_sample1.onclick = sample1;
   node_sample1.onmouseover = write_underline;
   node_sample1.onmouseout = clear_underline;*/
-  let swiper1 = new Swiper('.swiper-container', {
-    pagination: true,
+  // let swiper1 = new Swiper('.swiper-container', {
+  //   pagination: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   }
+  // });
+  const swiper = new Swiper('.swiper', {
     pagination: {
       el: '.swiper-pagination',
-      clickable: true
+      type: 'bullets',
     },
     navigation: {
       nextEl: '.swiper-button-next',
